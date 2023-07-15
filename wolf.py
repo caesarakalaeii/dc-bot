0545e8962719d0a9e6da2937b973f5a6ef33bdc0
caesarakalaeii <caesarlp@gmx.de>
2023-07-16 00:01:30 +0200

    added admin_pw to alex and wolf

--- wolf.py
+++ wolf.py
@@ -2,5 +2,5 @@ from bot import GPTBot
 from api_secrets import *
 
 if __name__ == '__main__':
-    bot = GPTBot(DISCORD_TOKEN_WOLF, OPENAI_API_KEY, "Winston Wolf", "Kangoshi", test_mode=True)
+    bot = GPTBot(DISCORD_TOKEN_WOLF, OPENAI_API_KEY, "Winston Wolf", "Kangoshi", test_mode=True, admin_pw=ADMIN_PASSWORT)
     bot.runBot()
