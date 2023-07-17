e9184cc05a1500c406df7b3fd42225a852230395
caesarakalaeii <caesarlp@gmx.de>
2023-07-18 01:53:29 +0200

    more customization

--- alex.py
+++ alex.py
@@ -2,5 +2,5 @@ from bot import GPTBot
 from api_secrets import *
 
 if __name__ == '__main__':
-    bot = GPTBot(DISCORD_TOKEN_ALEX, OPENAI_API_KEY, "Alex", "Caesar", stream_link = "https://twitch.tv/caesarlp", test_mode=False, admin_pw=ADMIN_PASSWORT, max_tokens = 64, temperature= 1)
+    bot = GPTBot(DISCORD_TOKEN_ALEX, OPENAI_API_KEY, "Alex", "Caesar", stream_link = "https://twitch.tv/caesarlp", test_mode=False, admin_pw=ADMIN_PASSWORT, max_tokens = 64, temperature= 1, timer_duration=60)
     bot.runBot()
