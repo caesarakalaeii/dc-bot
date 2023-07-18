3fada6b2783bb6759fc0656992c30e05ad275004
caesarakalaeii <caesarlp@gmx.de>
2023-07-18 19:22:56 +0200

    more tokens, to prevent, missing parts

--- wolf.py
+++ wolf.py
@@ -2,5 +2,5 @@ from bot import GPTBot
 from api_secrets import *
 
 if __name__ == '__main__':
-    bot = GPTBot(DISCORD_TOKEN_WOLF, OPENAI_API_KEY, "Winston Wolf", "Kangoshi", stream_link="https://twitch.tv/kangoshi_chan", test_mode=False, admin_pw=ADMIN_PASSWORT, max_tokens = 64, temperature= 1)
+    bot = GPTBot(DISCORD_TOKEN_WOLF, OPENAI_API_KEY, "Winston Wolf", "Kangoshi", stream_link="https://twitch.tv/kangoshi_chan", test_mode=False, admin_pw=ADMIN_PASSWORT, max_tokens = 128, temperature= 1)
     bot.runBot()
