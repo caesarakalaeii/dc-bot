3d73b1816ec2fe7b17d31892cbb618dfcab98a3c
caesarakalaeii <caesarlp@gmx.de>
2023-08-09 15:25:20 +0200

    updated winston to gpt-4

--- wolf.py
+++ wolf.py
@@ -3,5 +3,8 @@ from api_secrets import *
 
 if __name__ == '__main__':
 
-    bot = GPTBot(bot_token=DISCORD_TOKEN_WOLF, gpt_api_key=OPENAI_API_KEY, bot_name="Winston Wolf", streamer_name="Kangoshi",channel_id= 1136746061684293632,guild_id=789235412018135040, stream_link="https://twitch.tv/kangoshi_chan", test_mode=False, admin_pw=ADMIN_PASSWORT, max_tokens = 256, temperature= 1, timer_duration=100)
+    bot = GPTBot(bot_token=DISCORD_TOKEN_WOLF, gpt_api_key=OPENAI_API_KEY, bot_name="Winston Wolf", 
+                 streamer_name="Kangoshi",channel_id= 1136746061684293632,guild_id=789235412018135040, 
+                 stream_link="https://twitch.tv/kangoshi_chan", test_mode=False, admin_pw=ADMIN_PASSWORT, 
+                 max_tokens = 256, temperature= 1, timer_duration=100, model = "gpt-4")
     bot.runBot()
