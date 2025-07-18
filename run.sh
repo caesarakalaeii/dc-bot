959b657ec5ba652230ea9a379325ceb98e37e0e7
caesarakalaeii <truecaesarlp@gmail.com>
2025-07-18 21:22:58 +0200

    moved white/blacklist env var to new path

--- run.sh
+++ run.sh
@@ -2,17 +2,19 @@
 set -e
 
 # Decode base64-encoded whitelist and blacklist env vars
+mkdir -p /app/persistence
+
 if [ -n "$WHITE_LIST" ]; then
-    echo "$WHITE_LIST" | base64 -d > /app/whitelist_${BOT_NAME}.json
+    echo "$WHITE_LIST" | base64 -d > /app/persistence/whitelist_${BOT_NAME}.json
     echo "Whitelist for ${BOT_NAME} loaded:\n"
-    cat /app/whitelist_${BOT_NAME}.json
+    cat /app/persistence/whitelist_${BOT_NAME}.json
     echo "\n"
 fi
 
 if [ -n "$BLACK_LIST" ]; then
-    echo "$BLACK_LIST" | base64 -d > /app/blacklist_${BOT_NAME}.json
+    echo "$BLACK_LIST" | base64 -d > /app/persistence/blacklist_${BOT_NAME}.json
     echo "Blacklist for ${BOT_NAME} loaded:\n"
-    cat /app/blacklist_${BOT_NAME}.json
+    cat /app/persistence/blacklist_${BOT_NAME}.json
     echo "\n"
 fi
 
